fc3e8d2e373debc1d36b00f773a27117dfe132cb
Levi <joaolevi>
2021-11-18 23:14:43 -0300

    main functions created

--- grafo.c
+++ grafo.c
@@ -31,3 +31,58 @@ GRAFO *criaGrafo(int v) {
     for (i = 0; i < v; i++)
         g->adj[i].cab = NULL;
 }
+
+ADJACENCIA *criaAdj(int v, int peso) {
+    ADJACENCIA *temp = (ADJACENCIA *)malloc(sizeof(ADJACENCIA));
+    temp->vertice = v;
+    temp->peso = peso;
+    temp->prox = NULL;
+    return(temp);
+}
+
+bool criaAresta(GRAFO *gr, int vi, int vf, TIPOPESO p){
+    if (!gr) return(false);
+    if ((vf <0) || (vf >= gr->vertices))
+        return(false);
+    if ((vi<0) || (vi >= gr->vertices))
+        return(false);
+        ADJACENCIA *novo = criaAdj(vf, p);
+        novo->prox = gr->adj[vi].cab;
+        gr->adj[vi].cab = novo;
+        gr->arestas++;
+        return(true);
+}
+
+void imprime(GRAFO *gr){
+    print("Vertices: %d. Arestas %d. \n", gr->vertices, gr->arestas);
+    int i;
+    for(i = 0; i < gr->vertices; i++){
+        print("v%d: ", i);
+        ADJACENCIA *ad = gr->adj[i].cab;
+        while(ad){
+            print("v%d(%d) ", ad->vertice, ad->peso);
+            ad = ad->prox;
+        }
+        print("\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
